cf2c000f649c578ea5d639fb65ad0d1d4b8f03e4
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-01-23 13:45:08 +0200

    changed location -> latitude and longitude

--- app/controllers/brgr_controller.rb
+++ app/controllers/brgr_controller.rb
@@ -4,8 +4,9 @@ require 'json'
 
 class BrgrController < ApplicationController
   def show
-	@location = params[:messages][:location]
-	parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?' + @location + '&radius=5000&types=food&sensor=false&keyword=vegetarian&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
+	@latitude = params[:messages][:latitude]
+	@longitude = params[:messages][:longitude]
+	parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?' + @latitude + ',' + @longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 	http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 	http.use_ssl = true
 	request = Net::HTTP::Get.new(parsed_url.request_uri)

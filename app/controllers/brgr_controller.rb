ae5560cb163214031f9d50928f84a7e8131d9ad1
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-01-23 13:47:17 +0200

    Added missing location keyword from request url

--- app/controllers/brgr_controller.rb
+++ app/controllers/brgr_controller.rb
@@ -6,7 +6,7 @@ class BrgrController < ApplicationController
   def show
 	@latitude = params[:messages][:latitude]
 	@longitude = params[:messages][:longitude]
-	parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?' + @latitude + ',' + @longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
+	parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + @latitude + ',' + @longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 	http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 	http.use_ssl = true
 	request = Net::HTTP::Get.new(parsed_url.request_uri)

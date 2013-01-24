761ce1809314dd31a21be13fbb06f804f2338e09
Noemj <olli.rissanen@helsinki.fi>
2013-01-24 23:52:14 +0200

    Refactor model structure

--- app/controllers/brgr_controller.rb
+++ app/controllers/brgr_controller.rb
@@ -1,11 +1,12 @@
 class BrgrController < ApplicationController::Base
-  def show
-	@latitude = params[:messages][:latitude]
-	@longitude = params[:messages][:longitude]
-	@names = Restaurant.from_location @latitude @longitude
-  end
 
-  def add
-  end 
+	def show
+		@latitude = params[:messages][:latitude]
+		@longitude = params[:messages][:longitude]
+		@restaurant = Restaurant.from_location latitude, longitude
+	end
+
+	def add
+	end 
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,16 +4,33 @@ require 'json'
 
 class Restaurant < ActiveRecord::Base
 	
+	attr_reader :name, :latitude, :longitude
+
+	def initialize options
+		@name = options["name"]
+		@latitude = options["lat"]
+		@longitude = options["lng"]
+	end
+
+	def self.from_location latitude, longitude
+		restaurants = all_from_location latitude, longitude
+		restaurants.first
+	end
+
+	def self.all_from_location latitude, longitude
+		all_information = fetch_information latitude, longitude
+		format_results all_information["results"]
+	end
+
+	private
+
 	def self.from_location latitude, longitude
 		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + latitude + ',' + longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 		http.use_ssl = true
 		result = http.request(Net::HTTP::Get.new(parsed_url.request_uri))
-		parsed_json = JSON.parse result.body
-		format_results parsed_json["results"]
+		JSON.parse result.body
 	end
-
-	private
 
 	def self.format_results results
 		results.map{|result| result["name"]}

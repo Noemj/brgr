4d4c3bc51232541090cb0233a4c1d638f663d629
Noemj <olli.rissanen@helsinki.fi>
2013-01-24 23:56:09 +0200

    Changed the url to use constants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,8 @@ require 'json'
 
 class Restaurant < ActiveRecord::Base
 	
+	SEARCH_RADIUS = 5000
+
 	attr_reader :name, :latitude, :longitude
 
 	def initialize options
@@ -25,7 +27,7 @@ class Restaurant < ActiveRecord::Base
 	private
 
 	def self.from_location latitude, longitude
-		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + latitude + ',' + longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
+		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=#{latitude},#{longitude}&radius=#{SEARCH_RADIUS}&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 		http.use_ssl = true
 		result = http.request(Net::HTTP::Get.new(parsed_url.request_uri))

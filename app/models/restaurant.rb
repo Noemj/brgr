0e8481bab6f2a3f9160df96dd843a6fe78d27f75
Noemj <olli.rissanen@helsinki.fi>
2013-01-26 23:19:36 +0200

    Refactored radius back to string

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,8 +3,6 @@ require 'net/http'
 require 'json'
 
 class Restaurant < ActiveRecord::Base
-	
-	SEARCH_RADIUS = 5000
 
 	attr_reader :name, :latitude, :longitude
 
@@ -27,7 +25,7 @@ class Restaurant < ActiveRecord::Base
 	private
 
 	def self.fetch_information latitude, longitude
-		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=#{latitude},#{longitude}&radius=#{SEARCH_RADIUS}&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
+		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + latitude + ',' + longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 		http.use_ssl = true
 		result = http.request(Net::HTTP::Get.new(parsed_url.request_uri))

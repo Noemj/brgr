757a7fd1cb9b2f505b01a275152fb78306507968
Noemj <olli.rissanen@helsinki.fi>
2013-01-27 01:55:49 +0200

    Added geocode query for address

---
+++ app/models/geocode.rb
@@ -0,0 +1,13 @@
+class Geocode < ActiveRecord::Base
+
+	def self.from_address address
+		parsed_url = URI.parse('http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false')
+		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+		http.use_ssl = true
+		result = http.request(Net::HTTP::Get.new(parsed_url.request_uri))
+		parsed_json = JSON.parse result.body
+		latitude = parsed_json["results"]["geometry"]["location"]["lat"]
+		longitude = parsed_json["results"]["geometry"]["location"]["lng"]
+		return latitude longitude
+	end
+end

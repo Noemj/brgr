4604164c3821084afd43a0107a388d32afe52ed4
Noemj <olli.rissanen@helsinki.fi>
2013-01-26 21:02:04 +0200

    Added a missing @

--- app/controllers/brgr_controller.rb
+++ app/controllers/brgr_controller.rb
@@ -1,9 +1,9 @@
-class BrgrController < ApplicationController::Base
+class BrgrController < ApplicationController
 
 	def show
 		@latitude = params[:messages][:latitude]
 		@longitude = params[:messages][:longitude]
-		@restaurant = Restaurant.from_location latitude, longitude
+		@restaurant = Restaurant.from_location @latitude, @longitude
 	end
 
 	def add

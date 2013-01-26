281fa0335ee01e73559a421a91501c08a9f1dc09
Noemj <olli.rissanen@helsinki.fi>
2013-01-27 01:34:32 +0200

    Added drop down list for radius

--- app/controllers/brgr_controller.rb
+++ app/controllers/brgr_controller.rb
@@ -3,7 +3,8 @@ class BrgrController < ApplicationController
 	def show
 		@latitude = params[:messages][:latitude]
 		@longitude = params[:messages][:longitude]
-		@restaurant = Restaurant.from_location @latitude, @longitude
+		@radius = params[:messages][:radius]
+		@restaurant = Restaurant.from_location @latitude, @longitude, @radius
 	end
 
 	def add
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,28 +4,29 @@ require 'json'
 
 class Restaurant < ActiveRecord::Base
 
-	attr_reader :name, :latitude, :longitude
+	attr_reader :name, :latitude, :longitude, :radius
 
 	def initialize options
 		@name = options["name"]
 		@latitude = options["lat"]
 		@longitude = options["lng"]
+		@radius = options["rad"]
 	end
 
-	def self.from_location latitude, longitude
-		restaurants = all_from_location latitude, longitude
+	def self.from_location latitude, longitude, radius
+		restaurants = all_from_location latitude, longitude, radius
 		restaurants.first
 	end
 
-	def self.all_from_location latitude, longitude
-		all_information = fetch_information latitude, longitude
+	def self.all_from_location latitude, longitude, radius
+		all_information = fetch_information latitude, longitude, radius
 		format_results all_information["results"]
 	end
 
 	private
 
-	def self.fetch_information latitude, longitude
-		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + latitude + ',' + longitude + '&radius=5000&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
+	def self.fetch_information latitude, longitude, radius
+		parsed_url = URI.parse('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + latitude + ',' + longitude + '&radius=' + radius + '&types=food&sensor=false&key=AIzaSyC-CNui9DmsDGjlESFKa-iK42r1x11jywc')
 		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
 		http.use_ssl = true
 		result = http.request(Net::HTTP::Get.new(parsed_url.request_uri))
